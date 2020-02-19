edfc5f1b4446d224153c9c5ab8327a7337863b14
Elisa Nogueira <elisanog05@gmail.com>
2020-02-19 14:08:50 -0300

    Created Restaurant and Review models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
+# Restaurant model
 class Restaurant < ApplicationRecord
+  RESTAULIST = %w[chinese italian japanese french belgian].freeze
+
+  has_many :reviews, dependent: :destroy
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: RESTAULIST }
 end
