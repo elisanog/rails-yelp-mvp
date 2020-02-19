ec7791002e569fa478f81ad97a163aa44d7e2998
Elisa Nogueira <elisanog05@gmail.com>
2020-02-19 15:12:31 -0300

    Created the restaurants and reviews controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# Creating the routes
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: %i[index show new create] do
+    resources :reviews, only: %i[new create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+# This file should contain all the record creation needed to seed the database
+# with its default values.
+# The data can then be loaded with the rails db:seed command (or created
+# alongside the database with db:setup).
+
+Restaurant.destroy_all
+
+Restaurant.create!(name: 'Gregora', address: 'Gloria', category: 'italian',
+                   phone_number: '0631826040')
+
+Restaurant.create!(name: 'Kintaro', address: 'Paris', category: 'japanese')
+
+Restaurant.create!(name: 'Pomodoro', address: 'Roma', category: 'italian')
+
+Restaurant.create!(name: 'Panka', address: 'Gloria', category: 'chinese')
+
+Restaurant.create!(name: 'Piupiu', address: 'Porto', category: 'belgian')
