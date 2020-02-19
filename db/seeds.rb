dae3dcecc3c5af374cdf5ae778db87ca897b38a4
Elisa Nogueira <elisanog05@gmail.com>
2020-02-19 18:13:12 -0300

    Created the index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,9 @@
 # alongside the database with db:setup).
 
 Restaurant.destroy_all
-
 Restaurant.create!(name: 'Gregora', address: 'Gloria', category: 'italian',
                    phone_number: '0631826040')
-
 Restaurant.create!(name: 'Kintaro', address: 'Paris', category: 'japanese')
-
 Restaurant.create!(name: 'Pomodoro', address: 'Roma', category: 'italian')
-
 Restaurant.create!(name: 'Panka', address: 'Gloria', category: 'chinese')
-
 Restaurant.create!(name: 'Piupiu', address: 'Porto', category: 'belgian')
